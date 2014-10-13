633c607734d3a405ea47faeae8987e88b5466ffd
smlsunxie <smlsun@gmail.com>
2014-10-14 00:10:40 +0800

    refacter

--- src/dateGridTool.jsx
+++ src/dateGridTool.jsx
@@ -1,61 +1,114 @@
 /** @jsx React.DOM */
 var DateCell = React.createClass({
-  getInitialState: function() {
 
-    return {
-      day: this.props.day,
-      hour: this.props.hour,
-      selected: false
-    };
-  },
 
   hourMouseDown: function(){
-    if(this.state.selected === false)
-      this.setState({selected: true});
-    else this.setState({selected: false});
-
-    if(this.state.selected === true) this.setState({selectedClassName: "hourStatus-selected"});
-    else this.setState({selectedClassName: ""});
-
+    console.log("111");
+    if(this.props.selected === false)
+      this.setProps({selected: true});
+    else this.setProps({selected: false});
   },
-
-
 
   render: function() {
 
     var className = ""
-
-    if(this.props.day % 3 === 0) className = "_516n _516l _516m";
-    if(this.props.day % 3 === 1) className = "_516p _516l _516m";
-    if(this.props.day % 3 === 2) className = "_516o _516l _516m";
+    if(this.props.hour % 3 === 0) className = "_516n _516l _516m";
+    if(this.props.hour % 3 === 1) className = "_516p _516l _516m";
+    if(this.props.hour % 3 === 2) className = "_516o _516l _516m";
 
 
     var selectdClassName = "";
-    if(this.state.selected === true) selectdClassName = "hourStatus-selected"
-
-    if (selectdClassName !== "") className += " "+selectdClassName;
-
-    console.log("className", className);
+    if(this.props.selected === true) selectdClassName = "hourStatus-selected"
+    if(selectdClassName !== "") className += " " + selectdClassName;
 
 
-    return <div id="hourGrid" mousedown={this.hourMouseDown()} mouseover="hourMouseOver(hour)" className={className}></div>;
+    return <div id="hourGrid" onMouseDown={this.hourMouseDown} mouseover="hourMouseOver(hour)" className={className}></div>;
   }
 });
 
-var DateGridToolApp = React.createClass({
-  // getInitialState: function() {
-  //   return {items: [], text: ''};
-  // },
-  // onChange: function(e) {
-  //   this.setState({text: e.target.value});
-  // },
-  // handleSubmit: function(e) {
-  //   e.preventDefault();
-  //   var nextItems = this.state.items.concat([this.state.text]);
-  //   var nextText = '';
-  //   this.setState({items: nextItems, text: nextText});
-  // },
+var DateRow = React.createClass({
+
+
   render: function() {
+
+
+    var className = ""
+    if(this.props.day == 0) className = "_8-v _516k clearfix";
+    else if(this.props.day == 7) className = "_8-x _516k clearfix";
+    else className = "_516k clearfix";
+
+    var hours = []
+
+
+    for (var i = 0; i < this.props.hours.length; i++) {
+      hours.push(DateCell(this.props.hours[i]));
+    }
+
+
+    return (
+      <div className={className}>
+        <div className="bt-view bt-view _516l _516q">
+          <div className="_516r">{this.props.title}</div>
+        </div>
+
+        <div className="bt-view bt-view">
+          {hours}
+        </div>
+      </div>
+    );
+  }
+});
+
+
+
+var DateGridToolApp = React.createClass({
+  getInitialState: function() {
+    var initObject = {
+      week: [
+        { title: "Monday", hours: [], allday: {}}
+        , { title: "Tuesday", hours: [], allday: {}}
+        , { title: "Wednesday", hours: [], allday: {}}
+        , { title: "Thursday", hours: [], allday: {}}
+        , { title: "Friday", hours: [], allday: {}}
+        , { title: "Saturday", hours: [], allday: {}}
+        , { title: "Sunday", hours: [], allday: {}
+        }
+      ],
+      everyday: {
+        title: "Everyday",
+        hours: []
+      },
+
+      startHour: {},
+      overHour: {}
+    }
+
+    for (var i = 0; i < initObject.week.length; i++) {
+      for (var j = 0; j < 24; j++) {
+        initObject.week[i].hours.push({day:i, hour:j, selected: false});
+      }
+    }
+
+
+    return initObject;
+  },
+
+  handleHourMouseDown: function(hour){
+    this.setState({startHour: hour});
+  },
+  handleHourMouseOver: function(hour){
+    this.setState({overHour: hour});
+  },
+
+
+  render: function() {
+
+    var dateGrid = []
+
+    for (var i = 0; i < this.state.week.length; i++) {
+      dateGrid.push(DateRow(this.state.week[i]));
+    }
+
 
     return (
 
@@ -87,7 +140,8 @@ var DateGridToolApp = React.createClass({
           <div className="_8-z"></div>
           <div className="_8_p">All Day</div>
         </div>
-        <DateCell day="0" hour="0" />
+        {dateGrid}
+
       </div>
     );
   }
